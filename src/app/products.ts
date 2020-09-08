0fd086f16930af59658d5b93c12ccdd0d86799b8
lexiaoau <xiaoleau@gmail.com>
2020-09-08 20:02:54 +1000

    add route

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,15 +1,18 @@
 export const products = [
     {
+      productId: 1,
       name: 'Phone XL',
       price: 799,
       description: 'A large phone with one of the best screens'
     },
     {
+      productId: 2,
       name: 'Phone Mini',
       price: 699,
       description: 'A great phone with one of the best cameras'
     },
     {
+      productId: 3,
       name: 'Phone Standard',
       price: 299,
       description: ''
